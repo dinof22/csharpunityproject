a60190765f4bbb44794aec11fdcbefaa68193804
Unknown <jakeshmearlof4@gmail.com>
2016-12-14 14:26:42 -0700

    added other Enemy

--- csharpfinal/Assets/Scripts/Enemy2Controller.cs
+++ csharpfinal/Assets/Scripts/Enemy2Controller.cs
@@ -2,14 +2,41 @@
 using System.Collections;
 
 public class Enemy2Controller : MonoBehaviour {
+    public GameObject player;
+    public bool[] health = new bool[4] { false, false, false, false };
+    public int healthCounter;
+    public GameObject gameController;
 
-	// Use this for initialization
 	void Start () {
-	
+        healthCounter = 3;
+        gameController.GetComponent<EnemiesLeftController>().enemiesLeft.Add("Enemy2");
+        gameController.GetComponent<EnemiesLeftController>().CheckEnemies();
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
-	
+        CheckHealth();
 	}
+    void CheckHealth()
+    {
+        if (healthCounter <= 0)
+        {
+            health[0] = true;
+            gameController.GetComponent<KilltextController>().i = gameController.GetComponent<KilltextController>().i + 1;
+            gameController.GetComponent<EnemiesLeftController>().enemiesLeft.RemoveAt(0);
+            gameController.GetComponent<EnemiesLeftController>().CheckEnemies();
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                healthCounter = healthCounter - 1;
+            }
+        }
+    }
 }
